6b2e1c2500ac9e3a9bd500d9eb6e9d766b140248
JenMcD-Star <jenniferannvancura@gmail.com>
2023-10-23 17:09:00 -0500

    book titles completed

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,37 @@
 class Book
 # write your code here
+attr_accessor :title
+
+def initalize 
+    @title = title
+end
+
+
+def title=(book)
+    titleArray = []
+    excluded_words = ["the", "a", "an", "and", "in", "of", "on"]
+    array = book.split(" ")
+    
+array.each do |word|  
+    if word == ("i")
+        titleArray.push(word.capitalize)
+    
+    elsif excluded_words.include?(word)
+        titleArray.push(word)
+
+    elsif  word == array[-1] 
+        titleArray.push(word.capitalize ) 
+     
+    else
+         titleArray.push(word.capitalize )   
+     end
+     end
+     
+     first = titleArray.first.capitalize
+     titleArray[0] = first
+     titleArray = titleArray.join (" ")
+
+    @title = titleArray
+end
+
 end
