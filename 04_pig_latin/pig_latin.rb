ea96a85df6304e8e4c93c4f2afc6b546a02d1034
JenMcD-Star <jenniferannvancura@gmail.com>
2023-10-23 16:25:27 -0500

    pig latin completed

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,53 @@
 #write your code here
+
+def rules(string)
+    string = string.split("")
+    vowels = ["a", "A", "e", "E", "i", "I", "o","O", "u", "U", "y" "Y"]
+
+    if vowels.include?(string[0])
+        string.push("ay")
+        string.join("")
+    
+    elsif string.slice(0, 2).join("").include?("qu")
+        firstCon = string.shift(2)
+        string.push(firstCon, "ay")
+        string.join("")
+    
+    elsif string.slice(0, 3).join("") === "sch"  || string.slice(0, 3).join("").include?("qu")
+        firstCon = string.shift(3)
+        string.push(firstCon, "ay")
+        string.join("")
+
+    elsif vowels.include?(string[0]) == false && vowels.include?(string[1]) == false && vowels.include?(string[2]) == false
+        puts string
+        firstCon = string.shift(3)
+        string.push(firstCon, "ay")
+        string.join("")
+
+    elsif vowels.include?(string[0]) == false && vowels.include?(string[1]) == false
+        firstCon = string.shift(2)
+        string.push(firstCon, "ay")
+        string.join("")
+
+    else 
+        firstCon = string.shift()
+        string.push(firstCon, "ay")
+        string.join("")
+    end 
+end
+
+
+
+def translate(string)
+    multiples = []
+    if string.include?(" ")
+        string =  string.split(" ")
+        string.each do |word|
+           multiples.push(rules(word))
+        end
+    return multiples.join(" ")
+    
+    else
+        return rules(string) 
+end
+end
