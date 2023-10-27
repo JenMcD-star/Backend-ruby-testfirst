e6e78b913aaa2849aa666b46f027d2c74068b56a
JenMcD-Star <jenniferannvancura@gmail.com>
2023-10-26 19:36:53 -0500

    Refactored pig latin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,6 +1,37 @@
 #write your code here
 
-def rules(string)
+def translate(string)
+    words = string.split(' ')
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    twoLetters = ['qu', 'ch', 'br', 'th', 'bl', 'cr', 'st', 'tw', 'ph', 'sh', 'wh','kn']
+    threeLetters = ['sch', 'thr', 'squ', 'spr', 'scr', 'spl' ]
+
+
+      #map will check each word to see if it starts with a vowel,
+    words.map { |word|
+        if vowels.include?(word[0])
+          word = word + 'ay'
+
+    #see if the first 3 letter match the three letter exceptions
+        elsif threeLetters.include?(word[(0..2)])
+          word = word[(3..-1)]+word[(0..2)]+ 'ay'
+
+    #see if the first 2 letter match the two letter exceptions 
+        elsif twoLetters.include?(word[(0..1)])
+          word = word[(2..-1)]+word[(0..1)]+'ay'
+
+    #If doesn't meet any exceptions then it will take the word without the first letter, add the first letter then ay.
+        else
+          word[(1..-1)]+word[0]+'ay'
+        end
+    }.join(' ') #map returns an array so change it to a string
+
+end
+
+ 
+
+
+=begin def rules(string)
     string = string.split("")
     vowels = ["a", "A", "e", "E", "i", "I", "o","O", "u", "U", "y" "Y"]
 
@@ -19,7 +50,6 @@ def rules(string)
         string.join("")
 
     elsif vowels.include?(string[0]) == false && vowels.include?(string[1]) == false && vowels.include?(string[2]) == false
-        puts string
         firstCon = string.shift(3)
         string.push(firstCon, "ay")
         string.join("")
@@ -51,3 +81,4 @@ def translate(string)
         return rules(string) 
 end
 end
+=end
